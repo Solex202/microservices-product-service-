7c0fdfb02d209b0454d46e492af75c80aaa8d44c
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-01-18 23:47:51 +0100

    creating classes

--- src/main/java/com/lotaproject/productservice/controllers/ProductController.java
+++ src/main/java/com/lotaproject/productservice/controllers/ProductController.java
@@ -1,16 +1,25 @@
 package com.lotaproject.productservice.controllers;
 
 import com.lotaproject.productservice.dto.CreateProductRequest;
+import com.lotaproject.productservice.repository.ProductRepository;
+import com.lotaproject.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void  createProduct(@RequestBody CreateProductRequest createProductRequest){
+        productService.createProduct(createProductRequest);
+
+
 
 
     }
--- src/main/java/com/lotaproject/productservice/service/ProductServiceImpl.java
+++ src/main/java/com/lotaproject/productservice/service/ProductServiceImpl.java
@@ -1,12 +1,25 @@
 package com.lotaproject.productservice.service;
 
 import com.lotaproject.productservice.dto.CreateProductRequest;
+import com.lotaproject.productservice.model.Product;
+import com.lotaproject.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService{
+
+    private final ProductRepository productRepository;
     @Override
     public void createProduct(CreateProductRequest request) {
+        Product product = Product.builder()
+                .name(request.getName())
+                .price(request.getPrice())
+                .description(request.getDescription())
+                .build();
+
+        productRepository.save(product);
 
     }
 }

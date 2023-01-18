fe08e5d5dffca6ee0fad041867f52e1f9257c9e1
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-01-18 21:54:11 +0100

    creating classes

---
+++ src/main/java/com/lotaproject/productservice/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.lotaproject.productservice.service;
+
+import com.lotaproject.productservice.dto.CreateProductRequest;
+
+public interface ProductService {
+
+    public void createProduct(CreateProductRequest request);
+
+}
---
+++ src/main/java/com/lotaproject/productservice/service/ProductServiceImpl.java
@@ -0,0 +1,12 @@
+package com.lotaproject.productservice.service;
+
+import com.lotaproject.productservice.dto.CreateProductRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+    @Override
+    public void createProduct(CreateProductRequest request) {
+
+    }
+}
